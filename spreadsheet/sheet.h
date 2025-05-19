3f511c9ba40d16a72a4a3ff60128a68f4ddb1e78
aksi321 <96183002+aksi321@users.noreply.github.com>
2025-05-19 23:05:17 +0300

    Update sheet.h

--- spreadsheet/sheet.h
+++ spreadsheet/sheet.h
@@ -12,13 +12,14 @@ class Sheet : public SheetInterface {
 public:
     ~Sheet() override = default;
 
-    void               SetCell  (Position pos, std::string text) override;
+    void SetCell (Position pos, std::string text) override;
     const CellInterface* GetCell(Position pos) const override;
-    CellInterface*       GetCell(Position pos)       override;
-    void               ClearCell(Position pos)       override;
-    Size               GetPrintableSize()            const override;
-    void               PrintValues(std::ostream& out)const override;
-    void               PrintTexts (std::ostream& out)const override;
+    CellInterface* GetCell(Position pos) override;
+    void ClearCell(Position pos) override;
+    Size GetPrintableSize() const override;
+    void PrintValues(std::ostream& out)const override;
+    void PrintTexts (std::ostream& out)const override;
+    bool HasCircular(const Cell& start, const Cell& cur) const;
 
     friend class Cell;
 private:
@@ -26,15 +27,14 @@ private:
     std::vector<Row> cells_;
 
     static void CheckPosOrThrow(const Position& p);
-    void        EnsureExists (const Position& p);
+    void EnsureExists (const Position& p);
 
-    Cell*       GetOrCreate  (Position p);
+    Cell* GetOrCreate (Position p);
 
-    void        Link    (Cell& from, Cell& to);
-    void        UnlinkAll(Cell& node);
-    void        RebuildDeps(Cell& node,
+    void Link (Cell& from, Cell& to);
+    void UnlinkAll(Cell& node);
+    void RebuildDeps(Cell& node,
                             const std::vector<Position>& refs);
     mutable std::unordered_set<const Cell*> dfs_visited_;
-    bool        HasCircular(const Cell& start, const Cell& cur) const;
 
 };
